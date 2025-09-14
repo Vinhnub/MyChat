19e4cb39fbecf4c26bef7abba071bf7337e30e03
Vinh Nub <vinhnguyenvan1255@gmail.com>
2025-09-14 19:18:20 +0700

    v

--- server/server_call.py
+++ server/server_call.py
@@ -10,10 +10,11 @@ class VoiceServer(DatagramProtocol):
     def datagramReceived(self, data, addr):
         json_packet = base64.b64decode(data)
         packet = json.loads(json_packet.decode("utf-8"))
+        print(packet["username"], addr)
         if self.userOnline[packet["username"]]["groupCall"] == packet["groupName"]:
             self.clients[packet["groupName"]]["memberCall"][packet["username"]] = addr
 
         for client in self.clients[packet["groupName"]]["memberCall"]:
             if client != packet["username"]:
-                print(packet["username"], self.clients[packet["groupName"]]["memberCall"], client)
+                #print(packet["username"], self.clients[packet["groupName"]]["memberCall"], client)
                 self.transport.write(data, self.clients[packet["groupName"]]["memberCall"][client])
